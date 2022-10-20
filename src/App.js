fe4590caf77af574f40e5d1371568e5cdcc2fe33
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-19 17:06:34 -0700

    complete: 'making context dynamic'

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
     <AuthContext.Provider 
       value={{
         isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler
       }}
     >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
