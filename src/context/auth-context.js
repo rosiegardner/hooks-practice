7f7a70d7fd9afacc58e7a00fc0c6b53ffbee854b
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-19 17:30:52 -0700

    complete: 'building and using custom ctx provider component'

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // AuthContext is not a component
 // AuthContext is an object that will contain a component.
 
 const AuthContext = React.createContext({
-  isLoggedIn: false
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
 });
 
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  useEffect(() => {
+    const storeUserLogin = localStorage
+      .getItem('isLogginIn');
+
+    if(storeUserLogin === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+      }} >
+      {props.children}
+    </AuthContext.Provider>
+  )
+};
 export default AuthContext;
